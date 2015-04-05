3926e40023647c8234cb37ddf653ba3a853f14d6
Brandon Cen <bcen96@gmail.com>
2015-04-05 05:30:50 -0700

    reset

--- cookinglib.js
+++ cookinglib.js
@@ -1,34 +1,19 @@
 var CookingLibrary = function (){
 
-    //var type = "R";
-    // 0 = rest, 1 = mix, 2 = cut, 3 = heat
-    var type = 0;
-    var mixing_enabled = false;
-    var mixing_state = 1;
-    var mixing_min_x = 0.5;
-    var mixing_max_x = 0.7;
+    var type = "R";
 
+    var mixing_state = 1;
+    var mixing_min_x = -0.5;
+    var mixing_max_x = 0.5;
     var mixing_counter = 0;
 
-    var cutting_done = false;
-    var cutting_enabled = false;
     var cutting_state = 1;
     var cutting_min_yaw = 0;
     var cutting_max_yaw = 0.8;
     var cutting_counter = 0;
 
-    this.ToggleMixOn = function(){
-        mixing_enabled = true;
-    };
-    this.ToggleCutOn = function(){
-        cutting_enabled = true;
-    };
-    this.ToggleMixOff = function(){
-        mixing_enabled = false;
-    };
-    this.ToggleCutOff = function(){
-        cutting_enabled = false;
-    };
+    var task_count = 0;
+    var task_time = 0;
 
     this.SendMixSignal = function(x){
         if (mixing_state == 1){
@@ -52,11 +37,10 @@ var CookingLibrary = function (){
             if (yaw > cutting_max_yaw){
                 cutting_state = 1;
                 cutting_counter++;
-                cutting_done = true;
             }
         }
     };
-    /*this.CheckTask = function(){
+    this.CheckTask = function(){
         if (type == "R"){
             return true;
         }
@@ -81,63 +65,28 @@ var CookingLibrary = function (){
             }
         }
     };
-    */
     this.ReceiveSignal = function(x, y, z, pitch, yaw , row){
-        if (mixing_enabled){
-            if (mixing_state == 1){
-                if (x < mixing_min_x){
-                    mixing_state = 2;
-                }
-            } else {
-                if (x > mixing_max_x){
-                    mixing_state = 1;
-                    mixing_counter++;
-                }
-            }
+        if (type == "S"){
+            SendMixSignal(x);
         }
-        if (cutting_enabled){
-        //this.CheckTask();
-            if (cutting_state == 1){
-                if (yaw < cutting_min_yaw){
-                    cutting_state = 2;
-                }
-
-            } else{
-                if (yaw > cutting_max_yaw){
-                    cutting_state = 1;
-                    cutting_counter++;
-                }
-            }
+        if (type == "C"){
+            SendCutSignal(z);
         }
-        //this.CheckTask();
+        this.CheckTask();
     };
     this.GetCounter = function(){
-        if (mixing_enabled){
-            return mixing_counter;
-        } else if (cutting_enabled){
+        if (t == "C"){
             return cutting_counter;
+        } else if (t == "S"){
+            return mixing_counter;
         }
 
     };
-    this.GetCuttingDone = function(){
-        if (cutting_done){
-            cutting_done = false;
-            return true;
-        } else{
-            return false;
-        }
-    }
-    /*
+    
     this.NewTask = function(t, count, time){
         type = t.substring(0,1);
         task_count = count;
         task_time = time;
-        return type;
     };
-    */
-    this.ResetCounter = function(){
-        mixing_counter = 0;
-        cutting_counter = 0;
-    }
     
 };
